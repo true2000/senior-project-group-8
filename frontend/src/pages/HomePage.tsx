57663faeb685ab9c2ca3cf9393e8c9cd253d4b51
shark1112 <luke.lambert.1165@gmail.com>
2024-02-20 13:21:49 -0600

    added basic website workflow

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  const imageUrl =
+    'https://image.tmdb.org/t/p/original/oYuLEt3zVCKq57qu2F8dT7NIa6f.jpg';
+
+  return (
+    <div>
+      <img
+        src={imageUrl}
+  alt="Movie Poster"
+  style={{ maxWidth: '50%', height: 'auto' }}
+  />
+  </div>
+);
+};
+
+export default HelloWorld;
