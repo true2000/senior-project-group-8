55a8e61cd9d29b91ca561c22258aaec6ca0e6887
true2000 <57641483+true2000@users.noreply.github.com>
2024-02-20 13:34:30 -0600

    model data

--- model/model.py
+++ model/model.py
@@ -1,8 +1,8 @@
 # To install pytorch run the following type the following into a terminal
 # pip3 install torch torchvision torchaudio
 import torch
+import pandas as pd
 
-# Passes a random 5x3 matrix into torch model
-model = torch.rand(5,3)
-# Displays the model matrix
-print(model)
+data_frame = pd.read_csv("model/TMDB_movie_dataset_v11.csv")
+new_data_frame = data_frame[["release_date", "original_language","genres"]]
+#data = torch.fr(new_data_frame)
