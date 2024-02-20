effd9e035f19b0933b02e4a3a9a51f4d2e0e9dc3
shark1112 <luke.lambert.1165@gmail.com>
2024-02-20 13:24:05 -0600

    added webpages

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import '../styles/pages/HomePage.css';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
-const HelloWorld: React.FC = () => {
-  const imageUrl =
-    'https://image.tmdb.org/t/p/original/oYuLEt3zVCKq57qu2F8dT7NIa6f.jpg';
+const HomePage: React.FC = () => {
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleEnterClick = () => {
+    navigate('/movies'); // Navigate to your target path
+  };
 
   return (
-    <div>
-      <img
-        src={imageUrl}
-  alt="Movie Poster"
-  style={{ maxWidth: '50%', height: 'auto' }}
-  />
-  </div>
-);
+    <div className="homePageContainer">
+      <h1>FlickFinder</h1>
+      <p>
+        Find your next Flick based on movies that you are currently enjoying
+      </p>
+      <button onClick={handleEnterClick}>Enter</button>{' '}
+      {/* Button for navigation */}
+    </div>
+  );
 };
 
-export default HelloWorld;
+export default HomePage;
---
+++ frontend/src/pages/MoviePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/pages/MoviePage.css';
+import { useNavigate } from 'react-router-dom';
+
+const MoviePage: React.FC = () => {
+  const navigate = useNavigate(); // Hook for navigation
+  const handleEnterClick = () => {
+    navigate('/'); // Navigate to your target path
+  };
+  return (
+    <div className="moviePageContainer">
+      <h1>Movie Title</h1>
+      <button onClick={handleEnterClick}>exit</button>{' '}
+      {/* Button for navigation */}
+    </div>
+  );
+};
+
+export default MoviePage;
