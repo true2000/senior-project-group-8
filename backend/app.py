5ee0324ca63087810195100ae604f90f1d695aa3
shark1112 <luke.lambert.1165@gmail.com>
2024-02-22 13:25:51 -0600

    added swagger

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,26 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 from movie_dto import MovieDTO
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+
+# Swagger UI configuration
+SWAGGER_URL = '/swagger'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yml'  # URL for exposing the Swagger specification
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Movie API"
+    },
+)
 
 # Example movie data
 movies = [
@@ -43,6 +57,9 @@ class MovieList(Resource):
             response = {f"{prefix}_movies": movies_list}
 
         return response
+
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 api.add_resource(MovieList, '/movies')  # Adding the movies endpoint
 
