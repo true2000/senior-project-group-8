420ab3f99544188e32c9a6f1f20866cd24fc8940
true2000 <57641483+true2000@users.noreply.github.com>
2024-02-23 07:59:33 -0600

    filter data

--- model/model.py
+++ model/model.py
@@ -3,6 +3,14 @@
 import torch
 import pandas as pd
 
-data_frame = pd.read_csv("model/TMDB_movie_dataset_v11.csv")
-new_data_frame = data_frame[["release_date", "original_language","genres"]]
-#data = torch.fr(new_data_frame)
+def train():
+    # useing the idea of collaborative filtering
+    data_frame = pd.read_csv("model/TMDB_movie_dataset_v11.csv")
+    new_data_frame = data_frame[["id", "vote_average", "release_date", "original_language","genres"]]
+    # Displays the number of movies
+    print(new_data_frame.id.nunique())
+    # Rounds the user vote_average to a whole number
+    print(new_data_frame.vote_average.round(0).value_counts())
+
+    print(torch.sort(torch.Tensor(new_data_frame.vote_average.values)))
+train()
