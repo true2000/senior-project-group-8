d9a04aaeae714ede10231826b73866cb16160249
Prichnow <ptr5223@utulsa.edu>
2024-02-27 22:18:14 -0600

    Just small changes to HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -11,12 +11,17 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="homePageContainer">
+      <h1>IMAGE GOES HERE</h1>
       <h1>FlickFinder</h1>
       <p>
         Find your next Flick based on movies that you are currently enjoying
       </p>
       <button onClick={handleEnterClick}>Enter</button>{' '}
       {/* Button for navigation */}
+      <p>
+        This website is designed to help you find and increase your knowledge of
+        movies.
+      </p>
     </div>
   );
 };
