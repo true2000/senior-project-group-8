9f362d81f57f272dea5c4a5fe59e7e678633186c
true2000 <57641483+true2000@users.noreply.github.com>
2024-03-05 12:50:53 -0600

    model

--- model/model.py
+++ model/model.py
@@ -5,16 +5,20 @@ import torch
 # pip install pandas
 import pandas as pd
 
-def train():
+def train(data):
     # useing the idea of collaborative filtering
     data_frame = pd.read_csv("model/TMDB_movie_dataset_v11.csv")
-    new_data_frame = data_frame[["id", "vote_average", "release_date", "original_language","genres"]]
+    new_data_frame = data_frame[["id", "title", "vote_average", "release_date", "original_language","genres"]]
     # Displays the number of movies
-    print(new_data_frame.id.nunique())
+    ##print(new_data_frame.id.nunique())
     # Rounds the user vote_average to a whole number
-    print(new_data_frame.vote_average.round(0).value_counts())
+    ##print(new_data_frame.vote_average.round(0).value_counts())
+    for x in range (len(data)):
+        print(str(data[x]))
+        print(new_data_frame.loc[x, "title"])
+        #print(data[x])
 
-    print(torch.sort(torch.Tensor(new_data_frame.vote_average.values)))
+    ##print(torch.sort(torch.Tensor(new_data_frame.vote_average.values)))
 
-input = "Titanic"
-train()
+input = ["Interstellar", "Harry Potter and the Prisoner of Azkaban"]
+train(input)
