e8d2bc20c2ef82898e07b9a7ed3ef17a2c694ff7
dev0660 <dgp1496@utulsa.edu>
2024-03-12 13:25:55 -0500

    Added temporary movie data for front end

--- backend/movie_dto.py
+++ backend/movie_dto.py
@@ -1,11 +1,9 @@
 class MovieDTO:
     def __init__(self, name, director, image):
         self.name = name
-        self.director = director
         self.image = image
     def to_dict(self):
         return {
             "name": self.name,
-            "director": self.director,
             "image": self.image
         }
