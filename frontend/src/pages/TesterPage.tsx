92d2be88b96fec6bc1a9e61afa4ab58969ab3f98
Prichnow <ptr5223@utulsa.edu>
2024-03-14 12:02:34 -0500

    Trying to make movie pop up possible

--- frontend/src/pages/TesterPage.tsx
+++ frontend/src/pages/TesterPage.tsx
@@ -1,8 +1,45 @@
+import { useState } from 'react';
 import React from 'react';
 import '../styles/pages/TesterPage.css';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
+interface ImageInfo {
+  imageUrl: string;
+  title: string;
+  description: string;
+}
+
+const Popup: React.FC<ImageInfo & { onClose: () => void }> = ({
+  imageUrl,
+  title,
+  description,
+  onClose,
+}) => {
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <img src={imageUrl} alt={title} />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
 const TesterPage: React.FC = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleImageClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   const navigate = useNavigate(); // Hook for navigation
 
   const handleEnterClick = () => {
@@ -22,7 +59,7 @@ const TesterPage: React.FC = () => {
   };
 
   const posterPath = '/kyeqWdyUXW608qlYkRqosgbbJyK.jpg'; //Poster Pathway for avatar
-  const imageSize = 'w500'; // Choose the appropriate image size
+  const imageSize = 'w200'; // Choose the appropriate image size
   const baseURL = 'https://image.tmdb.org/t/p/'; //base URL for any of the pathways
 
   const fullURL = baseURL + imageSize + posterPath;
@@ -44,7 +81,21 @@ const TesterPage: React.FC = () => {
         </button>{' '}
       </div>
       <div className="pictureContainer">
-        <img src={fullURL} alt="MoviePoster"></img>
+        <img
+          className="normalProfile"
+          src={fullURL}
+          alt="MoviePoster"
+          onClick={handleImageClick}
+        ></img>
+        {/* Popup */}
+        {showPopup && (
+          <Popup
+            imageUrl={fullURL}
+            title="Avatar"
+            description="Blue People running around jumping on dragons and doing kinky things with their hair"
+            onClose={handleClosePopup}
+          />
+        )}
       </div>
     </div>
   );
