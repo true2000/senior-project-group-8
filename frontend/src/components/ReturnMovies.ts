aa0ecd503384de1f9f67114054e70a0dd7ca2e87
shark1112 <luke.lambert.1165@gmail.com>
2024-03-26 11:27:08 -0500

    steps needed to take to fix search bar

--- frontend/src/components/ReturnMovies.ts
+++ frontend/src/components/ReturnMovies.ts
@@ -3,3 +3,7 @@ export function searchStrings(query: string, data: string[]): string[] {
     item.toLowerCase().includes(query.toLowerCase()),
   );
 }
+
+//create an array of array one for each letter in the alphabet of data.
+//each array will contain the movies that start with that letter.
+//update the search string method to take the necessary array and return the movies that contain with the query.
