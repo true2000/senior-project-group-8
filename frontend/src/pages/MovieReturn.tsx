b52f92a5785740c321b89c4d4002efcf144259f8
Prichnow <ptr5223@utulsa.edu>
2024-04-12 10:48:29 -0500

    Adding Styling to return page

--- frontend/src/pages/MovieReturn.tsx
+++ frontend/src/pages/MovieReturn.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import '../styles/pages/MovieReturn.css';
 
 const MovieReturn = () => {
   const location = useLocation();
   const { movies } = location.state || {}; // Access state passed along with navigate
 
   return (
-    <div>
+    <div className="movieReturnContainer">
       <h1>Movie Comparison</h1>
       <pre>{JSON.stringify(movies, null, 2)}</pre>
     </div>
