8a2194dd9f4c5557e4bf8fca764e9f2d5d2f1588
Prichnow <ptr5223@utulsa.edu>
2024-04-18 13:12:23 -0500

    Adding Some styling to movie page

--- frontend/src/pages/MovieRecPage.tsx
+++ frontend/src/pages/MovieRecPage.tsx
@@ -7,54 +7,68 @@ const MovieRecPage = () => {
       <div className="movieRecHeader">
         <h1>Find Your Favourite Movies</h1>
       </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Action</h1>
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Action</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Action_top_50_movies.csv" />
+        </div>
       </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Action_top_50_movies.csv" />
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Adventure</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Adventure_top_50_movies.csv" />
+        </div>
       </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Adventure</h1>
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Crime</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Crime_top_50_movies.csv" />
+        </div>
       </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Adventure_top_50_movies.csv" />
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Drama</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Drama_top_50_movies.csv" />
+        </div>
       </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Crime</h1>
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Family</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Family_top_50_movies.csv" />
+        </div>
       </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Crime_top_50_movies.csv" />
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Horror</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Horror_top_50_movies.csv" />
+        </div>
       </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Drama</h1>
-      </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Drama_top_50_movies.csv" />
-      </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Family</h1>
-      </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Family_top_50_movies.csv" />
-      </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Horror</h1>
-      </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Horror_top_50_movies.csv" />
-      </div>
-      <div className="genreHeader">
-        {' '}
-        <h1>Science Fiction</h1>
-      </div>
-      <div className="movieRecContainer">
-        <MovieGenreLists csvFilePath="/genreList/Science Fiction_top_50_movies.csv" />
+      <div className="genreScrollContatiner">
+        <div className="genreHeader">
+          {' '}
+          <h1>Science Fiction</h1>
+        </div>
+        <div className="movieRecContainer">
+          <MovieGenreLists csvFilePath="/genreList/Science Fiction_top_50_movies.csv" />
+        </div>
       </div>
     </div>
   );
